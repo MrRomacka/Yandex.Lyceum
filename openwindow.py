861225377a5fdd1dc36e82ebffdd2f48c83b4385
Roman Dvgl <57865513+MrRomacka@users.noreply.github.com>
2019-12-14 13:10:38 +0500

    Added Classwork-X.

---
+++ openwindow.py
@@ -0,0 +1,8 @@
+import tkinter
+
+
+window = tkinter.Tk()
+window.title("Добро пожаловать в приложение PythonRu")
+canvas = window.Canvas(window, bg='blue', height=600, length=300)
+canvas.pack
+window.mainloop()
---
+++ Ткинтер.py
@@ -0,0 +1,8 @@
+import tkinter as tk
+
+
+master = tk.Tk()
+master.title("Добро пожаловать в приложение PythonRu")
+canvas = master.Canvas(master, bg='blue', height=600, length=300)
+canvas.pack
+master.mainloop()
